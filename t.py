459d8e15ec7e769fbd000b1c201c3a8963d4a09b
lxy878 <lxy878@yahoo.com>
2024-09-17 11:47:57 -0400

    file update

---
+++ t.py
@@ -0,0 +1,34 @@
+import math as ma
+# Print out format with variables by using f"{VARIABLE}"
+##word = "World"
+##print(f"Hello, {word}")
+
+# Typecasting int(), str(), float(), bool()
+##integer = 5
+##double = 2.1
+##boolean = False
+##string = "Hi"
+##print(bool(integer))
+
+# input()
+##data = input("Enter:")
+##print("Input is", data)
+
+# Math functions round(), abs(), pow() and import math
+##double = 2.6
+##print(round(double)) # 3
+##print(ma.floor(double))
+
+# If..elif..else
+##input_str=input("Enter a number:")
+##try:
+##    number = int(input_str) # also use str.isdigit()
+##    if number>0:
+##        print("Positive")
+##    elif number==0:
+##        print("Zero")
+##    else:
+##        print("Nagetive")
+##except ValueError:
+##    print("Input is Not a Number")
+
